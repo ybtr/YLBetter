6e22e413496623ceb17121509fce1665ed77d03e
browjob <32989588+browjob@users.noreply.github.com>
2021-03-10 15:32:46 +0200

    Test AutoUpdate

--- ylbetter.user.js
+++ ylbetter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YTLBetter Button
 // @namespace    https://ylbetter.github.io
-// @version      1.0
+// @version      1.1
 // @description  Try to take over the world!
 // @icon         https://avatars.githubusercontent.com/u/80352165?s=200&v=4
 // @author       browjob
